b1120f0cd35e35bf59cfffbd740001ba8d919205
Shubham Samarth <shubhams@heaptrace.net>
2024-06-10 17:22:11 +0530

    Sidebar layout fixed

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,15 @@
-import imgPath from "../assets/multikart.png";
+import imgPath from "../assets/muliticart_logo.png";
 
 export default function Sidebar() {
   return (
-    <div className="w-[500px]">
-      <img src={imgPath} alt="MultiKart_logo" height="80px" />
-      <h1 className="bg-gray-950">Main Menu</h1>
+    <div>
+      <img
+        className="mx-auto"
+        src={imgPath}
+        alt="MultiKart_logo"
+        height="80px"
+      />
+      <h1>Main Menu</h1>
       <div>
         <a href="">Dashboard</a>
         <a href="">Users</a>
@@ -12,29 +17,3 @@ export default function Sidebar() {
     </div>
   );
 }
-
-// import React from "react";
-// import imgPath from "../assets/multikart.png";
-
-// export default function Sidebar() {
-//   return (
-//     <div className="w-[500px] bg-gray-100 p-4">
-//       <img src={imgPath} alt="MultiKart_logo" height="80px" />
-//       <h1 className="bg-gray-950 text-white p-2">Main Menu</h1>
-//       <div className="mt-2 space-y-2">
-//         <a
-//           href=""
-//           className="block text-gray-700 hover:bg-gray-200 p-2 rounded"
-//         >
-//           Dashboard
-//         </a>
-//         <a
-//           href=""
-//           className="block text-gray-700 hover:bg-gray-200 p-2 rounded"
-//         >
-//           Users
-//         </a>
-//       </div>
-//     </div>
-//   );
-// }
--- src/components/UserWidget.jsx
+++ src/components/UserWidget.jsx
@@ -1,13 +1,17 @@
 import useUserStore from "../useUserStore";
 import UserCard from "./UserCard";
+import Sidebar from "./Sidebar";
 
 export default function UserWidget() {
   const users = useUserStore((state) => state.users);
 
   return (
-    <div>
-      {/* <Sidebar /> */}
-      <div className="flex gap-1">
+    <div className="flex h-screen">
+      <div className="w-1/5 h-full">
+        <Sidebar />
+      </div>
+      <div className="w-4/5 h-full p-4 grid lg:grid-cols-3 gap-4">
+        {/* <div className="w-4/5 h-full p-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4"></div> */}
         {users.map((user) => (
           <UserCard key={user.id} user={user} />
         ))}
